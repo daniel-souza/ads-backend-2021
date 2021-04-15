a061220011a26a2fe44eea4ce84f9bd3fa19047c
daniel-souza <dssouzadan@gmail.com>
2021-04-15 19:35:00 -0300

    Atividade em sala

---
+++ 15-exercicio-api-carros-com-auth-seguranca/src/routes.js
@@ -0,0 +1,39 @@
+//const { Router } = require("express");
+import { Router } from "express";
+
+import ProductController from "./controllers/ProductController.js";
+import authMiddleware from "./middlewares/auth.js";
+import UserController from "./controllers/UserController.js";
+import LoginController from "./controllers/LoginController.js";
+
+const routes = new Router();
+
+// rotas
+routes.get("/", async (req, res) => {
+  res.send("Olá mundo!");
+});
+
+/**
+ * 1. Criar as rotas de carros, conforme especificações do exercício
+ * 2. Icluir o middleware authMiddleware nas rotas de carros, para garantir que essas
+ * operações sejam realizadas apenas a usuários autenticados.
+ */
+
+
+/* Login */
+/** Criar a rota de login com o método post. */
+
+/* User */
+routes.post("/users", UserController.create);
+
+/**
+ * Se a autenticação estiver funcionando adequadamente, pode utilizar essas rotas como teste.
+ */
+
+/* Me (usuário autenticado) */
+routes.get("/me", authMiddleware, UserController.listMe);
+routes.put("/me", authMiddleware, UserController.updateMe);
+routes.delete("/me", authMiddleware, UserController.deleteMe);
+
+//module.exports = routes;
+export default routes;
